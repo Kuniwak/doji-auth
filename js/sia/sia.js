3374b2aa282780f6f45b551a47294cd62326d902
OrgaChem <orga.chem.job@gmail.com>
2013-01-23 23:22:03 +0900

    Add root namespace script

---
+++ js/sia/sia.js
@@ -0,0 +1,19 @@
+// This script licensed under the MIT.
+// http://orgachem.mit-license.org
+
+/**
+ * @fileoverview The base script for SIA (Simultaneous Inputable Authentication).
+ * @author orga.chem.job@gmail.com (OrgaChem)
+ */
+
+goog.provide('sia');
+goog.require('goog.dom');
+goog.require('goog.events');
+goog.require('goog.object');
+goog.require('goog.ui.Component');
+goog.require('sia.ui.NumericalKeypad');
+
+var NUMERICAL_KEYPAD_ID = 'sia-buttons';
+var numKeys = new sia.ui.NumericalKeypad();
+numKeys.decorate(goog.dom.getElement(NUMERICAL_KEYPAD_ID));
+window.NUM_KEYS = numKeys;
