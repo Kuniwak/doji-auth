ead9c5e0e3614a6264d662b946fee63335ec2a0a
OrgaChem <orga.chem.job@gmail.com>
2013-01-24 15:31:13 +0900

    Add a class for AuthenticatorHelper

---
+++ js/sia/auth/authhelper.js
@@ -0,0 +1,48 @@
+// This script licensed under the MIT.
+// http://orgachem.mit-license.org
+
+/**
+ * @fileoverview The script for authentication module for SIA (Simultaneous
+ *   Inputable Authentication).
+ * @author orga.chem.job@gmail.com (OrgaChem)
+ */
+
+goog.provide('sia.auth.AuthenticationHelper');
+goog.require('goog.asserts');
+
+
+/**
+ * A class for authentication helper.
+ *
+ * @constructor
+ * @param {*} opt_correct Autheticator is an any object has {@code a.equals(b)}
+ *   method.
+ */
+sia.auth.AuthenticationHelper = function(opt_correct) {
+	this.acceptEncryption = !!opt_acceptEncyption;
+
+	if (opt_correct) {
+		this.setCorrect(opt_correct);
+	}
+};
+
+
+/**
+ * Sets a indentification.
+ * @param {*} correct Autheticator is an any object has {@code a.equals(b)}
+ *   method.
+ */
+sia.auth.AuthenticationHelper.prototype.setCorrect = function(correct) {
+	this.correct_ = correct;
+};
+
+
+/**
+ * Authenticates a authenticator.
+ * @param {*} auth The autheticator.
+ * @return {boolean} Whether authenticator was matched.
+ */
+sia.auth.AuthenticationHelper.prototype.authenticate = function(auth) {
+	goog.asserts.assert(this.correct_ && this.correct_.equals);
+	return this.correct_.equals(auth);
+};
