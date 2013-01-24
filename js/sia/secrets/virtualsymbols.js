412fc2c1f67fe29ea9c8214336b6ac9d0a605c97
OrgaChem <orga.chem.job@gmail.com>
2013-01-25 03:27:25 +0900

    Fix a bug about only virtual symbols pushing

--- js/sia/secrets/virtualsymbols.js
+++ js/sia/secrets/virtualsymbols.js
@@ -22,7 +22,8 @@ goog.require('goog.structs.Map');
 sia.secrets.VirtualSymbol = {
 	LARGE: 'L',
 	MEDIUM: 'M',
-	SMALL: 'S'
+	SMALL: 'S',
+	UNDEFINED: '*'
 };
 
 
@@ -57,6 +58,7 @@ sia.secrets.resolveSet = function(set) {
 	var Symbol = sia.secrets.VirtualSymbol;
 	var sorted = [];
 	var map = new goog.structs.Map();
+	map.set('*', 0)
 
 	goog.structs.forEach(set, function(symbol) {
 		switch (symbol) {
@@ -78,29 +80,26 @@ sia.secrets.resolveSet = function(set) {
 
 	var larget, medium, smaller;
 	if (hasLarger) {
-		larger = goog.array.peek(sorted);
-		goog.asserts.assertString(larger);
+		larger = goog.array.peek(sorted) || Symbol.UNDEFINED;
 		map.set(larger, map.get(larger) + 1);
 	}
 	if (hasSmaller) {
-		smaller = sorted[0];
-		goog.asserts.assertString(smaller);
+		smaller = sorted[0] || Symbol.UNDEFINED;
 		map.set(smaller, map.get(smaller) + 1);
 	}
 	if (hasMedium) {
 		var len = sorted.length;
 		switch (len) {
 			case 3:
-				medium = sorted[1];
+				medium = sorted[1] || Symbol.UNDEFINED;
 				break;
 			case 2:
-				medium = sorted[hasLarger ? 1 : 0];
+				medium = sorted[hasLarger ? 1 : 0] || Symbol.UNDEFINED;
 				break;
 			default:
-				medium = sorted[0];
+				medium = sorted[0] || Symbol.UNDEFINED;
 				break;
 		}
-		goog.asserts.assertString(medium);
 		map.set(medium, map.get(medium) + 1);
 	}
 
